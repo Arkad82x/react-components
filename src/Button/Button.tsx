eff918e45be23d32301760818155fdfc18d78326
Andre Brandt <andre.brandt@campaign.plus>
2020-04-28 13:33:44 +0200

    added dummy button

---
+++ src/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import MuiButton from '@material-ui/core/Button'
+
+const Button = () => {
+    return <MuiButton variant="contained" color="primary"> Test Button </MuiButton>
+}
+
+export default Button
